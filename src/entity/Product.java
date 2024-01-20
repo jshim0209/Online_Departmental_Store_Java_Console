2594cfe83611273272f8135fa19d47011b76d1fa
jshim0209 <100379451+jshim0209@users.noreply.github.com>
2024-01-19 22:31:33 -0500

    Entities implemented

--- src/entity/Product.java
+++ src/entity/Product.java
@@ -1,4 +1,76 @@
 package entity;
 
+import java.util.Objects;
+
 public class Product {
+
+    private int productId;
+    private String productName;
+    private double sellingPrice;
+    private int availableQuantity;
+
+    public Product() {
+    }
+
+    public Product(int productId, String productName, double sellingPrice, int availableQuantity) {
+        this.productId = productId;
+        this.productName = productName;
+        this.sellingPrice = sellingPrice;
+        this.availableQuantity = availableQuantity;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getSellingPrice() {
+        return sellingPrice;
+    }
+
+    public void setSellingPrice(double sellingPrice) {
+        this.sellingPrice = sellingPrice;
+    }
+
+    public int getAvailableQuantity() {
+        return availableQuantity;
+    }
+
+    public void setAvailableQuantity(int availableQuantity) {
+        this.availableQuantity = availableQuantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return productId == product.productId && Double.compare(product.sellingPrice, sellingPrice) == 0 && availableQuantity == product.availableQuantity && Objects.equals(productName, product.productName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productId, productName, sellingPrice, availableQuantity);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId=" + productId +
+                ", productName='" + productName + '\'' +
+                ", sellingPrice=" + sellingPrice +
+                ", availableQuantity=" + availableQuantity +
+                '}';
+    }
 }
