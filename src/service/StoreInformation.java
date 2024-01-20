d1ac62312a9cbbe0873f247aa2080aac24d8ebfe
jshim0209 <100379451+jshim0209@users.noreply.github.com>
2024-01-19 22:58:47 -0500

    Application Console Menu Implemented

--- src/service/StoreInformation.java
+++ src/service/StoreInformation.java
@@ -1,4 +1,21 @@
 package service;
 
 public class StoreInformation {
+    public void getAllProducts() {
+    }
+
+    public void getProductById() {
+    }
+
+    public void getProductsByCategory() {
+    }
+
+    public void getProductByName() {
+    }
+
+    public void getTotalAmountSpent() {
+    }
+
+    public void getProfitAmountByCategory() {
+    }
 }
